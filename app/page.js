af7539736c6cc56c73b086b811e52aab9ea60e73
pranayaryal <drpranayaryal@gmail.com>
2023-06-21 00:29:25 -0400

    add rct search

--- app/page.js
+++ app/page.js
@@ -5,6 +5,17 @@ export default function Home() {
   return (
     <div className="mb-32 flex flex-col lg:grid lg:mb-0 lg:grid-cols-2 lg:text-left">
       <a
+        href="/pubmed/search-pubmed-randomized-controlled-trial"
+        className="group rounded-lg border border-transparent px-0 py-2 lg:px-5 lg:py-4 transition-colors hover:border-gray-300 hover:bg-gray-50"
+      >
+        <h2 className={`mb-2 md:mb-3 lg:text-2xl font-semibold`}>
+          Search PubMed{' '}
+        </h2>
+        <p className={`lg:block m-0 max-w-[30ch] opacity-50`}>
+         Search PubMed for a Randomized Controlled Trial 
+        </p>
+      </a>
+      <a
         href="/pubmed"
         className="group rounded-lg border border-transparent px-0 py-2 lg:px-5 lg:py-4 transition-colors hover:border-gray-300 hover:bg-gray-50"
       >
